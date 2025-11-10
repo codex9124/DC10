bf0258c1db4e8730c1b0839bb964a50895b0655a
codex9124 <akhade.avi.dhule@gmail.com>
2025-11-10 06:56:41 +0530

    Create info theory.c

---
+++ info theory.c
@@ -0,0 +1,11 @@
+clear;
+clear;
+clc;
+dmin=5
+ // (s+1)<= dmin number errors can be detected(s)
+ s=dmin-1;
+printf(" i)Number of detected errors \n\n \t s <= %d  ",s );
+//(2t+1)<=dmin number errors can be corrected(t)
+t=(dmin-1)/2;
+printf("\n\n ii) Number of corrected errors\n\n \t  t<= %d ",t );
+
